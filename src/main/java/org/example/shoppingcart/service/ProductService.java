ab56d02408ce7d2bd4bc49e3459c58964f610df8
ducdosomething <duclehoang211201@gmail.com>
2024-05-24 14:35:09 +0700

    demo shopping cart

---
+++ src/main/java/org/example/shoppingcart/service/ProductService.java
@@ -0,0 +1,24 @@
+package org.example.shoppingcart.service;
+
+import org.example.shoppingcart.model.Product;
+import org.example.shoppingcart.repository.IProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProductService implements IProductService{
+    @Autowired
+    private IProductRepository productRepository;
+
+    @Override
+    public Iterable<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
+}
